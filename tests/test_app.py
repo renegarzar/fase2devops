d3a58a5dc438d716053ab01265d7abb626124675
Rene <rene@10.0.2.15>
2025-03-13 18:27:12 -0600

    Añadi archivo a carpeta tests

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import pytest
+from app.main import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 404  # No hay ruta raíz definida
