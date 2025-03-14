6baba8adf5443883ffde33e150daaae9e967a0cd
EC2 Default User <ec2-user@ip-172-31-6-25.us-east-2.compute.internal>
2025-03-14 07:51:44 +0000

    Simulación de errores en AWS

--- app/database.py
+++ app/database.py
@@ -5,13 +5,5 @@ DB_FILE = "clientes.db"
 def init_db():
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS clientes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre TEXT NOT NULL,
-            correo TEXT UNIQUE NOT NULL,
-            telefono TEXT NOT NULL
-        )
-    ''')
     conn.commit()
     conn.close()
